75bf494b73d385e1e1dd23b3b8b106e53d75ce1a
Matt Conway <mattwigway@gmail.com>
2012-07-05 14:37:48 -0700

    MetroArea load api working.

--- app/controllers/MetroAreas.java
+++ app/controllers/MetroAreas.java
@@ -2,6 +2,28 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import models.MetroArea;
+import utils.GeometryUtils;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.io.ParseException;
 
-public class MetroAreas extends CRUD {
+public class MetroAreas extends Controller {
+    /**
+     * Create a new Metro Area
+     * @param name The name of this metro area
+     * @param geometry The EWKT geometry of this metro area
+     */
+    public static void create (String name, String geometry) {
+        //String name = params.get("name");
+        //String geometry = params.get("geometry");
+        MultiPolygon the_geom;
+
+        try {
+            the_geom = (MultiPolygon) GeometryUtils.parseEwkt(geometry);
+        } catch (ParseException e) {
+            return;
+        }
+
+        new MetroArea(name, the_geom).save();
+    }
 }
