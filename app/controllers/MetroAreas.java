55f8cfcbc81e4f716a73b50608eb6e00878b5af7
Matt Conway <mattwigway@gmail.com>
2012-07-05 15:26:08 -0700

    Generating correct GeoJSON.

--- app/controllers/MetroAreas.java
+++ app/controllers/MetroAreas.java
@@ -2,10 +2,12 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.db.jpa.JPA;
 import models.MetroArea;
 import utils.GeometryUtils;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.io.ParseException;
+import javax.persistence.Query;
 
 public class MetroAreas extends Controller {
     /**
@@ -25,5 +27,25 @@ public class MetroAreas extends Controller {
         }
 
         new MetroArea(name, the_geom).save();
+    }
+
+    /**
+     * Return a MetroArea as GeoJSON
+     */
+    public static void get (int id) {
+        // max decimal places 6 (per demory), include GeoJSON short CRS (2)
+        // TODO: is FROM MetroArea the right thing to do here?
+        String query = "SELECT m.name, ST_AsGeoJSON(m.the_geom, 6, 2) AS geom " +
+            "FROM MetroArea m WHERE m.id = ?";
+        Query q = JPA.em().createNativeQuery(query);
+        q.setParameter(1, id);
+        Object[] result = (Object[]) q.getResultList().get(0);
+
+        // TODO: writing JSON by hand is ugly
+        String output = "{\"properties\": {\"name\": \"" + (String) result[0] + "\"}," +
+            "\"geometry\":" + (String) result[1] + "," + 
+            "\"type\": \"Feature\"}";
+        
+        renderJSON(output);
     }
 }
