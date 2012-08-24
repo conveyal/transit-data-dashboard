b8726ec1421f32e1cdd213e23ffffd4cd78cbe08
Matt Conway <mattwigway@gmail.com>
2012-08-24 08:40:50 -0700

    More defaultBikesAllowed.

---
+++ app/models/DefaultBikesAllowedType.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Are bikes allowed on this agency? Values are YES, NO and WARN, which indicates that we aren't 
+ * sure and the user needs to check.
+ * @author matthewc
+ *
+ */
+public enum DefaultBikesAllowedType {
+    YES, NO, WARN;
+}
