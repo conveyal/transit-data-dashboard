e89bd719348dcaa4dd5e39935275b58f8f028471
Matt Conway <mattwigway@gmail.com>
2012-08-28 07:31:11 -0700

    equals() and hashCode() for GtfsFeed (based off IDs only).

--- app/models/GtfsFeed.java
+++ app/models/GtfsFeed.java
@@ -209,4 +209,15 @@ public class GtfsFeed extends Model implements Cloneable {
     	return ret;
     			
     }
+    
+    public int hashCode () {
+        return id.hashCode();
+    }
+    
+    public boolean equals(Object o) {
+        if (!(o instanceof GtfsFeed))
+            return false;
+        
+        return ((GtfsFeed) o).id == this.id;
+    }
 }
