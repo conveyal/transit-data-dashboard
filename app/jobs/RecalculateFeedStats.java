efd6452e5f2d42c8bc1049f3f319e9bfb4465f2f
Matt Conway <mattwigway@gmail.com>
2012-09-11 11:30:14 -0700

    Error checking during stats calculation.

--- app/jobs/RecalculateFeedStats.java
+++ app/jobs/RecalculateFeedStats.java
@@ -4,28 +4,36 @@ import java.io.File;
 
 import models.FeedParseStatus;
 import models.GtfsFeed;
+import play.Logger;
 import play.db.jpa.NoTransaction;
 import play.jobs.Job;
 import play.modules.spring.Spring;
 import updaters.FeedStatsCalculator;
 import updaters.FeedStorer;
+import updaters.UpdaterFactory;
 
 public class RecalculateFeedStats extends Job {
     public void doJob () {
         FeedStatsCalculator stats;
         File feedFile;
-        FeedStorer storer = Spring.getBeanOfType(FeedStorer.class);
+        UpdaterFactory updaterFactory = Spring.getBeanOfType(UpdaterFactory.class);
+        FeedStorer storer = updaterFactory.getStorer();
         
         for (GtfsFeed feed : GtfsFeed.<GtfsFeed>findAll()) {
             if (feed.status == FeedParseStatus.FAILED)
                 // no reason to reparse a failed feed
                 continue;
             
-            feedFile = storer.getFeed(feed.storedId); 
+            feedFile = storer.getFeed(feed.storedId);
             try {
                 stats = new FeedStatsCalculator(feedFile);
                 stats.apply(feed);
-            } catch (Exception e) {}
+            } catch (Exception e) {
+                Logger.error("Error calculating feed stats for agency %s, this must be an OBA " +
+                		"change or file system error", feed.agencyName);
+                e.printStackTrace();
+                continue;
+            }
             
             storer.releaseFeed(feed.storedId);
             
