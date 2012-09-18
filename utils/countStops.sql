f0e71c2cb0261ba1487277133fbb467262384c89
Matt Conway <mattwigway@gmail.com>
2012-09-18 15:02:02 -0700

    An SQL script to count stops-per-metro in the DB.

---
+++ utils/countStops.sql
@@ -0,0 +1,13 @@
+﻿-- Calculate the number of stops in each metro, and sort descending
+SELECT m.id, m.name, sum(g.stops)
+  FROM MetroArea m
+    JOIN MetroArea_NtdAgency mn ON (m.id = mn.metroarea_id)
+    JOIN NtdAgency n ON (mn.agencies_id = n.id)
+    JOIN NtdAgency_GtfsFeed ng ON (ng.ntdagency_id = n.id)
+    JOIN GtfsFeed g ON (ng.feeds_id = g.id)
+  WHERE g.supersededBy_id IS NULL
+    AND g.status = 'SUCCESSFUL'
+    AND NOT g.disabled
+  GROUP BY m.id
+  ORDER BY 3 DESC;
+  
